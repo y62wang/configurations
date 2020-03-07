8663c6fbc513138254dab446a0708d0653d0a502
Wang, Yang <yanwany@amazon.com>
2020-03-06 18:42:46 -0800

    upate common_aliases

--- common_aliases.sh
+++ common_aliases.sh
@@ -1,2 +1,3 @@
 alias gdc='git diff --cached'
 alias geffort='git log --pretty=format: --name-only | sort | uniq -c | sort -rg'
+alias glarge='git ls-tree -l -r --full-name --abbrev HEAD | sort -n -k 4 -r'
